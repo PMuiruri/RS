1b46c849361792eae318ad6767bf2ff2a30c0989
Pennina Muiruri <ext-peninah.muiruri@metacoregames.com>
2026-03-19 12:15:50 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import createNextIntlPlugin from 'next-intl/plugin'
 import type { NextConfig } from 'next'
 
-const withNextIntl = createNextIntlPlugin()
+const withNextIntl = createNextIntlPlugin('./localeConfig/request.ts')
 
 const nextConfig: NextConfig = {}
 
